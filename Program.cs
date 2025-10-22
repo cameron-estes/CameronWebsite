26a700418b3654bed8ddf5bede8dad50869230c8
Cameron Estes <cameron.estes@domain.spas.k.vu>
2025-10-21 19:08:57 -0500

    fixing project 5 errors with retrieving assets from proper root and changing Program.cs to allow for porper MIME types

--- Program.cs
+++ Program.cs
@@ -2,7 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseStaticFiles();
+// Configure static files with custom MIME types
+var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
+provider.Mappings[".obj"] = "application/octet-stream";
+provider.Mappings[".amc"] = "application/octet-stream";
+provider.Mappings[".asf"] = "application/octet-stream";
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
+
+// app.UseStaticFiles();
+
 app.MapGet("/", () => Results.Redirect("/index.html"));
 
 app.Run();
